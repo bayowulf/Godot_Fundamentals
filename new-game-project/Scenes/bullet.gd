02a55c0306e90c19541939608edb9d9d050560c6
bayowulf <jvgallagheriv@gmail.com>
2025-04-25 19:14:44 -0400

    more collisions

--- new-game-project/Scenes/bullet.gd
+++ new-game-project/Scenes/bullet.gd
@@ -7,3 +7,15 @@ var direction: Vector2 = Vector2()
 
 func _physics_process(delta: float) -> void:
 	position += direction.normalized() * SPEED * delta
+
+
+func _on_area_entered(area: Area2D) -> void:
+	queue_free()
+	
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Crate:
+		body.destroy()
+	queue_free() 
+		
+	
--- new-game-project/Scenes/pickup.gd
+++ new-game-project/Scenes/pickup.gd
@@ -1,2 +1,7 @@
 extends Area2D
 class_name Pickup
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Tank:
+		queue_free()
