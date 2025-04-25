d8302ecf4f274528616b6bef683b5b1fec82436f
bayowulf <jvgallagheriv@gmail.com>
2025-04-24 21:54:58 -0400

    initial commit

---
+++ new-game-project/Scenes/ui/menus/main_menu.gd
@@ -0,0 +1,27 @@
+extends Control
+##MainMenu.tscn
+
+signal start_game()
+@onready var buttons_v_box = %ButtonsVBox
+
+func _ready() -> void:
+	focus_button()
+
+func _onstart_game_button_pressed() -> void:
+	start_game.emit()
+	hide()
+	
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+	
+func _on_visibility_changed() -> void:
+	if visible:
+		focus_button()
+		
+func focus_button() -> void:
+	if buttons_v_box:
+		var button: Button = buttons_v_box.get_child(0)
+		if button is Button:
+			button.grab_focus()
+	
+	
