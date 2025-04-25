d8cc02f36452d39ad296e972a1d7ecc567adaa71
bayowulf <jvgallagheriv@gmail.com>
2025-04-25 13:55:24 -0400

    third video in series

---
+++ new-game-project/Scenes/tank.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+const SPEED = 64.0
+const TURN_SPEED = 2
+const ROTATE_SPEED = 20
+
+@export var weapon : Weapon
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var body_sprite: Sprite2D = $BodySprite
+@onready var collider: CollisionShape2D = $CollisionShape2D
+
+var direction := Vector2.RIGHT
+
+func _physics_process(delta: float) -> void:
+	var input_direction := Input.get_vector("turn_left","turn_right","move_backward","move_forward")
+	if input_direction.x != 0:
+		#rotate direction based on our input vector and applyturn speed
+		#NOTE 'direction' and 'rotation' are Node2D properties
+		direction = direction.rotated(input_direction.x * (PI/2) * TURN_SPEED * delta)
+		rotation = direction.angle()
+	if input_direction.y != 0:
+		# Move in a forward/backward direction and play animation
+		animation_player.play("move")
+		#NOTE 'velocity' is a Vector2D property
+		velocity = lerp(velocity, (direction.normalized() * input_direction.y) * SPEED, SPEED * delta)
+	else:
+		velocity = Vector2.ZERO
+		animation_player.play("idle")
+	
+	# Apply our movement velocity
+	move_and_slide()
+	
+	# Apply weapon rotation
+	var weapon_rotate_direction := Input.get_axis("rotate_weapon_left","rotate_weapon_right")
+	weapon.rotation_degrees += (weapon_rotate_direction * ROTATE_SPEED * delta * PI)
+	
+		
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("weapon_fire"):
+		weapon.fire()
+	
---
+++ new-game-project/Scenes/weapon.gd
@@ -0,0 +1,34 @@
+extends Node2D
+class_name Weapon
+# by adding class name to a script you improve better integration with godot's engine like autocomplete and better visibility
+
+enum STATES { READY, FIRING, RELOADING }
+
+@export var BULLET_SCENE: PackedScene
+
+@onready var reload_timer: Timer = $ReloadTimer
+
+var state: STATES = STATES.READY
+
+func change_state(new_state: STATES):
+	state = new_state
+	
+func fire():
+	if state == STATES.FIRING || state == STATES.RELOADING:
+		return
+		
+	change_state(STATES.FIRING)
+	# Create a bullet at our position and set its direction
+	var bullet = BULLET_SCENE.instantiate()
+	bullet.direction = Vector2.from_angle(global_rotation)
+	bullet.global_position = global_position
+	# Add bullt to root scene so that translation is in world space
+	get_tree().root.add_child(bullet)
+	
+	#set our state to reload and start out timer
+	change_state(STATES.RELOADING)
+	reload_timer.start()
+	
+
+func _on_reload_timer_timeout() -> void:
+	change_state(STATES.READY)
